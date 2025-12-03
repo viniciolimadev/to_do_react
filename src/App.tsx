238e2df8c7f90f684db0af8d3ec4fd61ae514f66
viniciolimadev <dev.vinicio@gmail.com>
2025-12-03 16:06:32 -0300

    Criando componente Text ,adicionando Icons ao projeto e instanlando cva

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import Text from "./components/text";
 
 function App() {
-  return (<div className="text-pink-dark">Hello World</div>)
+  return (
+    <div className="flex flex-col gab-2">
+      <Text variant="body-sm-bold" className="text-pink-dark">
+        Hello World
+      </Text>
+      <Text variant="body-md" className="text-pink-dark">
+        Hello World
+      </Text>
+      <Text variant="body-md-bold" className="text-pink-dark">
+        Hello World
+      </Text>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/text.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+export const textVariants = cva("font-sans text-gray-400", {
+  variants: {
+    variant: {
+      "body-sm-bold": "text-sm leading-5 font-semibold",
+      "body-md": "text-base leading-6 font-normal",
+      "body-md-bold": "text-base leading-6 font-semibold",
+    },
+  },
+  defaultVariants: {
+    variant: "body-md",
+  },
+});
+
+interface TextProps extends VariantProps<typeof textVariants> {
+  as?: keyof React.JSX.IntrinsicElements;
+  className?: string;
+  children?: React.ReactNode;
+}
+
+function Text({
+  as = "span",
+  variant,
+  className,
+  children,
+  ...props
+}: TextProps) {
+  return React.createElement(
+    as,
+    {
+      className: textVariants({ variant, className }),
+      ...props,
+    },
+    children
+  );
+}
+
+export default Text;
