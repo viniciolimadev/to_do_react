dc52ca7cdb2ef80838351924be4d2e35126e21fb
viniciolimadev <dev.vinicio@gmail.com>
2025-12-03 17:01:10 -0300

    Criando componente Icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
-import Text from "./components/text";
+import Text from "./components/Text";
+import Icon from "./components/Icon";
+
+import TrashIcon from "./assets/icons/trash.svg?react";
+import PencilIcon from "./assets/icons/pencil.svg?react";
+import CheckIcon from "./assets/icons/check.svg?react";
+import SpinnerIcon from "./assets/icons/spinner.svg?react";
+import XIcon from "./assets/icons/x.svg?react";
+import PlusIcon from "./assets/icons/plus.svg?react";
 
 function App() {
   return (
@@ -12,6 +20,14 @@ function App() {
       <Text variant="body-md-bold" className="text-pink-dark">
         Hello World
       </Text>
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-pink-dark" />
+        <Icon svg={PencilIcon} className="fill-pink-dark" />
+        <Icon svg={CheckIcon} className="fill-pink-dark" />
+        <Icon svg={SpinnerIcon} className="fill-pink-dark" animate />
+        <Icon svg={XIcon} className="fill-pink-dark" />
+        <Icon svg={PlusIcon} className="fill-pink-dark" />
+      </div>
     </div>
   );
 }
---
+++ src/components/Icon.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+export const iconVariants = cva("", {
+  variants: {
+    animate: {
+      false: "",
+      true: "animate-spin",
+    },
+  },
+  defaultVariants: {
+    animate: false,
+  },
+});
+
+interface IconProps
+  extends React.ComponentProps<"svg">,
+    VariantProps<typeof iconVariants> {
+  svg: React.FC<React.ComponentProps<"svg">>;
+}
+
+function Icon({ svg: SvgComponent, animate, className, ...props }: IconProps) {
+  return (
+    <SvgComponent className={iconVariants({ animate, className })} {...props} />
+  );
+}
+
+export default Icon;
